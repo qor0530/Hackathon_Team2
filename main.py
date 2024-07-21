5c5c72cf2a8505e131bc32d8031e5e06d471250e
Paik JongHwa <qorwhdghk12@gmail.com>
2024-07-21 23:28:06 +0900

    INIT signup
    signup 페이지 기본 html css

--- main.py
+++ main.py
@@ -27,5 +27,10 @@ async def my_page(request: Request):
 async def my_page(request: Request):
     return templates.TemplateResponse(request=request, name="login.html")
 
+
+@app.get("/signup", response_class=HTMLResponse)
+async def my_page(request: Request):
+    return templates.TemplateResponse(request=request, name="signup.html")
+
 # API 연결
 # (추후 개발)
