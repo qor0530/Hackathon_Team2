479c1de270de18002ef5d27a5eb3cf09715b94bd
unsag <unsagaxma@naver.com>
2024-07-28 02:25:08 +0900

    User 모델 추가

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import *
 from config.database import Base
 
+
 class Quiz(Base):
     __tablename__ = 'quizzes'
 
@@ -12,3 +13,20 @@ class Quiz(Base):
     answer = Column(String, index=True)
     hint = Column(String, index=True)
     answer_explanation = Column(Text, index=True)
+
+
+class User(Base):
+    __tablename__ = 'users'
+ 
+    id = Column(Integer, primary_key=True)
+    login_id = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    nickname = Column(String, unique=True, nullable=False)
+    profile_image = Column(String)
+    learning_history = Column(Text)  
+    total_learning_time = Column(Float, default=0.0) 
+    level = Column(Integer, default=1)
+    ranking_score = Column(Float, default=0.0)
+    subscription = Column(Boolean, default=False)
+    ranking = Column(Integer, default=0)
+    attendance = Column(Integer, default=0)
