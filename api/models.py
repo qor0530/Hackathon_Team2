561478903d529d40e045d691d145ffa796fdcc48
Paik JongHwa <qorwhdghk12@gmail.com>
2024-07-27 15:22:30 +0900

    backend 기본 세팅

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import *
+from config.database import Base
+
+class Quiz(Base):
+    __tablename__ = 'quizzes'
+
+    id = Column(Integer, primary_key=True, index=True)
+    level = Column(Integer, index=True)
+    subject = Column(String, index=True)
+    sentence = Column(Text, index=True)
+    explanation = Column(Text, index=True)
+    answer = Column(String, index=True)
+    hint = Column(String, index=True)
+    answer_explanation = Column(Text, index=True)
