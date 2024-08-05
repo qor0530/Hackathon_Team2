ef75878d72540fb26dcccd0150c375ee82960586
Paik JongHwa <qorwhdghk12@gmail.com>
2024-08-05 19:15:34 +0900

    ai

--- domain/Gemini.py
+++ domain/Gemini.py
@@ -1,14 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, APIRouter
 from pydantic import BaseModel
 import google.generativeai as genai
-from fastapi import APIRouter, Form, Depends, Request, Cookie, HTTPException
-
 
 router = APIRouter(prefix="/gemini")
 
 # Google API 키 설정
 GOOGLE_API_KEY = "AIzaSyCHADfi0J8bptpzZOUWP_3sXyGU2kBcL7w"
 genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 
 class PromptRequest(BaseModel):
@@ -25,15 +24,13 @@ async def grade_essay(request: PromptRequest):
 
     try:
         # Google Generative AI를 사용한 문법 검사 및 피드백 생성
-        prompt = f"문제: {problem}\n내용: {content}\n답변: {
-            answer}\n\n이 답변의 문법적 오류를 찾아 수정하고, 개선할 점을 알려주세요."
-        response = genai.generate(
-            model="gemini-1.5-flash",
-            prompt=prompt,
-        )
-        feedback = response['generations'][0]['text']
+        prompt = f"문제: {problem}\n내용: {content}\n답변: {answer} 이에 대한 피드백을 작성해줘"
 
-        return {"answer": answer, "feedback": feedback}
+        # 텍스트 생성 요청 보내기
+        response = model.generate_content(prompt)
 
+        # response에서 필요한 내용을 추출하는 부분이 필요할 수 있음
+        feedback = response.text
+        return {"feedback": feedback}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
